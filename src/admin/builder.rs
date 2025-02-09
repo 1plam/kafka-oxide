38637a9466788b7c63e5fe340630a778be0b475e
1plam <77026826+1plam@users.noreply.github.com>
2025-02-09 11:42:29 +0100

    feat: Implement default builder pattern

--- src/admin/builder.rs
+++ src/admin/builder.rs
@@ -1,4 +1,4 @@
-use super::types::AdminClient;
+use super::types::{AdminClient, DefaultAdminClient};
 use crate::error::Error;
 use rdkafka::ClientConfig;
 
@@ -14,13 +14,28 @@ impl AdminClientBuilder {
     }
 
     pub fn bootstrap_servers(mut self, servers: &[&str]) -> Self {
-        assert!(!servers.is_empty(), "Bootstrap servers list cannot be empty");
         self.config.set("bootstrap.servers", servers.join(","));
         self
     }
 
+    pub fn client_id(mut self, client_id: &str) -> Self {
+        self.config.set("client.id", client_id);
+        self
+    }
+
+    pub fn set(mut self, key: &str, value: &str) -> Self {
+        self.config.set(key, value);
+        self
+    }
+
     pub async fn build(self) -> Result<AdminClient, Error> {
-        let admin_client = self.config.create()?;
-        Ok(AdminClient::new(admin_client).await?)
+        let admin_client: DefaultAdminClient = self.config.create()?;
+        AdminClient::new(admin_client).await
+    }
+}
+
+impl Default for AdminClientBuilder {
+    fn default() -> Self {
+        Self::new()
     }
 }
